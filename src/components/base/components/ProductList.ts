fe6235a55cb8acdd3984390c53d213f25ac7dd40
Asreny Docenko <arsenijdocenko102@gmail.com>
2025-12-01 22:54:22 +0300

    base classes and API client

---
+++ src/components/base/components/ProductList.ts
@@ -0,0 +1,28 @@
+import { IProduct } from "../../../types";
+
+class ProductList {
+    protected _items: IProduct[] = [];
+    protected _previewItem: IProduct | null = null;
+
+    saveItems(items: IProduct[]): void  {
+        this._items = [...items]
+    }
+
+    getItems(): IProduct[] {
+        return [...this._items]
+    }
+
+    getItem(id: string): IProduct | undefined {
+        return this._items.find(item => item.id === id);
+    } 
+
+    savePreviewItem(item: IProduct): void {
+         this._previewItem = item;
+    }
+
+    getPreviewItem(): IProduct | null {
+        return this._previewItem ? this._previewItem : null;
+    }
+}
+
+export { ProductList };
--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,60 @@
+
+import { Cart } from './components/base/components/Cart';
+import { ProductList } from './components/base/components/ProductList';
 import './scss/styles.scss';
+import { apiProducts } from './utils/data';
+import { ApiClient } from './components/base/Api';
+import { Api } from './components/base/Api';
+import { API_URL } from './utils/constants'; 
+import { IProductsResponse as ProductResponse } from './types';
+
+
+
+// Проверка методов ProductList
+const ProductsModel = new ProductList();
+ProductsModel.saveItems(apiProducts.items);
+console.log('Массив товаров из каталога: ', ProductsModel.getItems());
+ProductsModel.getItem(ProductsModel.getItems()[0].id);
+console.log('Полученный товар по id: ', ProductsModel.getItem(ProductsModel.getItems()[0].id));
+ProductsModel.savePreviewItem(ProductsModel.getItems()[0]);
+console.log('Предпросмотр товара: ', ProductsModel.getPreviewItem());
+
+
+// Проверка методов Cart
+const CartModel = new Cart();
+CartModel.addItem(ProductsModel.getItems()[0]);
+console.log('Товары в корзине: ', CartModel.getItems());
+CartModel.removeItem(ProductsModel.getItems()[0]);
+console.log('Товары в корзине после удаления: ', CartModel.getItems());
+CartModel.addItem(ProductsModel.getItems()[1]);
+console.log('Общая стоимость товаров в корзине: ', CartModel.getTotalPrice());
+console.log('Общее количество товаров в корзине: ', CartModel.getTotalCount());
+console.log('Проверка наличия товара в корзине по id: ', CartModel.checkItemInCart(ProductsModel.getItems()[1].id));
+
+
+
+const api = new Api(API_URL);
+const apiClient = new ApiClient(api);
+let ProductFromApi: ProductResponse;
+
+// Тест getProductList
+ apiClient.getProductList()
+    .then(products => {console.log('Товары получены:', products.total)
+         ProductFromApi = products;
+    })
+    .catch(error => console.error('Ошибка getProductList:', error));
+
+// Тест createOrder
+const testOrder = {
+    payment: 'online' as const,
+    email: 'test@test.ru',
+    phone: '+79998887766',
+    address: 'Тестовый адрес',
+    total: 1450,
+    items: ['c101ab44-ed99-4a54-990d-47aa2bb4e7d9']
+};
+
+
+apiClient.createOrder(testOrder)
+    .then(result => console.log('Результат заказа:', result))
+    .catch(error => console.error('Ошибка createOrder:', error));
